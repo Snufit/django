b624a871915e97140d90b9872f9191640bcea4c6
Snufit <antakov7@yahoo.com>
2025-11-27 13:10:10 +0700

    Первые шаги достигнуты, добавлена ошибка 404 и О программе

--- phase_comparison_protection/main/urls.py
+++ phase_comparison_protection/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name='home'), # http://127.0.0.1:8000/main/
     path("about/", views.about, name='about'),
+    path("login/", views.login, name = 'login'),
+    path("register/", views.register, name = 'register'),
 #    path("notfound/", views.error, name='notfound'), # Представление ошибки
 ]
